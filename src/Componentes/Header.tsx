f872f805baf53bb424c2a778c68acad49a8b6d33
EdMarzal97 <edsanchezmarzal@gmail.com>
2023-12-02 11:26:21 -0500

    video working

--- src/Componentes/Header.tsx
+++ src/Componentes/Header.tsx
@@ -1,1 +1,11 @@
+const Header = () => {
+  return (
+    <>
+      <div>
+        <video src={require("../../public/video.mp4")} autoPlay muted loop />
+      </div>
+    </>
+  );
+};
 
+export default Header;
