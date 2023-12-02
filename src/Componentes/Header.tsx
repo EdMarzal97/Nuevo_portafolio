d9a1464ea55fdae801159d2338ab1da48a476b62
EdMarzal97 <edsanchezmarzal@gmail.com>
2023-12-02 14:55:46 -0500

    header finished with resume download included

--- src/Componentes/Header.tsx
+++ src/Componentes/Header.tsx
@@ -1,9 +1,22 @@
+import styles from "../styles/header.module.css"
+
 const Header = () => {
   return (
     <>
-      <div>
-        <video src={require("../../public/video.mp4")} autoPlay muted loop />
-      </div>
+      <main className={styles.main}>
+        <div className={styles.contents}>
+          <h1>
+            Edward S. Marzal
+          </h1>
+          <div className={styles.buttons}>
+              <h2>Software Engineer</h2>
+          </div>
+          <div className={styles.buttons}>
+            <button> <a href="/Edward Marzal resume.pdf" download="Edward Marzal resume"> Download my resume </a> </button>
+          </div>
+        </div>
+        <video src={require("../../public/video.mp4")} autoPlay muted loop className={styles.video} />
+      </main>
     </>
   );
 };
